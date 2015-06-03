34d03c5927da7105d063b79dcc5b38bcd9885318
Niharika <niharika_jayanthi@yahoo.co.in>
2015-06-03 10:52:29 +0530

    Code with comments

--- Task-3/distance.py
+++ Task-3/distance.py
@@ -1,19 +1,30 @@
+# Import cv2 and matplotlib
 import cv2
 import matplotlib.pyplot as plt
 
+# Read image and convert it to grayscale
 img = cv2.imread("example.jpg")
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
+# Convert to binary image using thresholding
 ret, th = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
-cv2.imshow("Binary",th)
-
+# Perform distance transformation
 distance = cv2.distanceTransform(th, cv2.cv.CV_DIST_L2,5)
 
+# Create new window
+plt.figure(0)
+
+# Plot grayscale image on 1st plot
+plt.subplot(121)
+plot = plt.imshow(gray)
+plot.set_cmap('bone')
+
+# Plot distance transformed image on 2nd plot
+plt.subplot(122)
 plot = plt.imshow(distance)
 plot.set_cmap('bone')
-plt.colorbar()
+
+# Display window
 plt.show()
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
