bacd0f1618ad32d57bc382a71f923e005f74cedf
Dheeraj22 <dheeraj.kamath@yahoo.com>
2015-06-08 11:05:36 +0530

    Images slightly changed

--- Task-3/Image Filtering/Gaussian Blur/GaussFilter.py
+++ Task-3/Image Filtering/Gaussian Blur/GaussFilter.py
@@ -1,16 +1,33 @@
 #Import OpenCV and Numpy
-import numpy
+import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 #Read the image
 img = cv2.imread('pepper.png')
 
+'''
+OpenCV represents RGB images as multi-dimensional NumPy arrays…but in reverse
+order!This means that images are actually represented in BGR order rather than
+RGB!
+'''
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   #Coverting from bgr to rgb
+
+
 # Do the processing
 blur = cv2.GaussianBlur(img,(5,5),0)
 
-# Show the image
-cv2.imshow('Gaussian',blur)
-cv2.imshow('Original',img)
+
+plt.figure(0)       # Create a new window
+
+plt.subplot(121),plt.imshow(img), plt.title('Original')
+plt.xticks([]), plt.yticks([])      #Removing ticks
+
+plt.subplot(122),plt.imshow(blur), plt.title('Gaussian')
+plt.xticks([]), plt.yticks([])
+
+plt.show()          #Display the window
+
 
 # Close and exit
 cv2.waitKey(0)
